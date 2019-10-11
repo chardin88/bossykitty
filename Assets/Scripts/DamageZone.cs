39e563968aa2449b2ead2a114c6756b61102e698
chardin88 <chris.hardin@loop.colum.edu>
2019-10-10 21:41:14 -0500

    Blocking, collectibiles, damage zones, and enemies
    Added in blocks to make water inaccesible, strawberries to gain health, damage zones (spikes) and robot enemies.

---
+++ Assets/Scripts/DamageZone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageZone : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Fox_Controller controller = other.GetComponent<Fox_Controller>();
+
+        if(controller != null)
+        {
+            controller.ChangeHealth(-1);
+        }
+    }
+}
